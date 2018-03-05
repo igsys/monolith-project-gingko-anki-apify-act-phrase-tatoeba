d2b67366dd67c496a117a28bfa940171a5304118
Michael Chan <michael@innova-global.com>
2018-03-05 23:41:51 +0900

    Build - limited to 2, working | next: continue

--- main.js
+++ main.js
@@ -24,13 +24,6 @@ const getLanguage = language => {
 }
 
 const getWordCount = str => {
-    // let totalSoFar = 0
-    // for (var i = 0; i < WordCount.length; i++) {
-    //     if (str[i] === ' ') { // if a space is found in str
-    //         totalSoFar += 1 // add 1 to total so far
-    //     }
-    // }
-    // return totalsoFar += 1 // add 1 to totalsoFar to account for extra space since 1 space = 2 words
     return str.split(' ').length
 }
 
