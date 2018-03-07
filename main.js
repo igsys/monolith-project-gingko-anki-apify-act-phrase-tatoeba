179405b8a53f79c2e4d67b15d5c951a2f1fcaeb2
Michael Chan <michael@innova-global.com>
2018-03-08 03:00:51 +0900

    Build - updated | next: continue

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ const typeCheck = require('type-check').typeCheck
 const puppeteer = require('puppeteer')
 const cheerio = require('cheerio')
 
+// counter for max number of entries
+var counter = 0
+const ENTRIES_MAX = 3
+const WORDS_MIN = 3
+const PAGES_MAX = 2
+
 // Definition of the input
 const INPUT_TYPE = `{
-    query: String,
-    source: String,
-    dictionary: Maybe String
+    query: String
+    source: String
+    translation: String
 }`
 
 const LEVEL_TYPE = {
@@ -19,6 +25,15 @@ const LEVEL_TYPE = {
 const getLanguage = language => {
     switch (language) {
         case 'french': return 'fra'
+        case 'english': return 'eng'
+        case 'russian': return 'rus'
+        case 'spanish': return 'spa'
+        case 'italian': return 'ita'
+        case 'chinese': return 'cmn'
+        case 'japanese': return 'jpn'
+        case 'german': return 'deu'
+        case 'korean': return 'kor'
+        case 'latin': return 'lat'
         default: return ''
     }
 }
@@ -29,7 +44,7 @@ const getWordCount = str => {
 
 const getPageContent = async (uri, browser, counter) => {
     // only limit to certain number of entries
-    if (counter === 2) return definitions
+    if (counter === ENTRIES_MAX) return definitions
     const page = await browser.newPage()
     await page.goto(uri, {
         timeout: 200000,
@@ -41,11 +56,12 @@ const getPageContent = async (uri, browser, counter) => {
     let definitions = []
     $('.sentence-and-translations').each((i, elem) => {
         // only limit to certain number of entries
-        if (counter === 2) return definitions
+        if (counter === ENTRIES_MAX) return definitions
         const phrase_mono = $(elem).find('.sentence.layout-row').eq(0).find('.text.flex').text().trim()
+        // TODO: remove other puntuation marks like ? ! with regex
         const phrase_mono_puntuation_removed = phrase_mono.replace('!', '').trim()
         // capture phrases with words greater than defined number
-        if (getWordCount(phrase_mono_puntuation_removed) >= 3) {
+        if (getWordCount(phrase_mono_puntuation_removed) >= WORDS_MIN) {
             const phrase_tran = $(elem).find('.translation.layout-row').eq(0).find('.text.flex').text().trim()
             definitions.push({
                 meaning: phrase_tran,
@@ -56,7 +72,7 @@ const getPageContent = async (uri, browser, counter) => {
                     phrase_tran
                 }]
             })
-            counter += 1
+            counter = counter + 1
         }
     })
     await page.close()
@@ -83,11 +99,12 @@ Apify.main(async () => {
     const browser = await launchPuppeteer()
 
     // Navigate to each Tatoeba.org page
-    var counter = 0
     let definitions = []
-    for (i = 1; i < 3; i++) {
-        const uri = `https://tatoeba.org/eng/sentences/search/page:${i}?query=${input.query}&from=${getLanguage(input.source)}&to=eng&orphans=no&unapproved=no&native=yes&user=&tags=&list=&has_audio=&trans_filter=limit&trans_to=eng&trans_link=&trans_user=&trans_orphan=&trans_unapproved=&trans_has_audio=&sort=words`
-        const response = await getPageContent(uri, browser, counter)
+    for (i = 0; i < PAGES_MAX; i++) {
+        const uri = `https://tatoeba.org/${getLanguage(input.translation)}/sentences/search/page:${i + 1}?query=${input.query}&from=${getLanguage(input.source)}&to=${getLanguage(input.translation)}&orphans=no&unapproved=no&native=yes&user=&tags=&list=&has_audio=&trans_filter=limit&trans_to=${getLanguage(input.translation)}&trans_link=&trans_user=&trans_orphan=&trans_unapproved=&trans_has_audio=&sort=words`
+        console.log(uri)
+        // Limit to max number of entries
+        const response = counter < ENTRIES_MAX ? await getPageContent(uri, browser, counter) : []
         definitions = definitions.concat(response)
     }
 
@@ -98,6 +115,7 @@ Apify.main(async () => {
         input,
         definitions
     }
-    console.log('output:', output)
+    console.log('output:')
+    console.dir(output)
     await Apify.setValue('OUTPUT', output)
 })
